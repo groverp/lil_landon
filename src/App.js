535fdb2a75d39ee5dd2f48b53fc9cde1418def22
Parvinder Grover <pgrover@opengov.com>
2021-04-07 01:22:00 -0700

    Added JSON for data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Body from './components/Body.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in less than a day.
-           <br/>
-          Accepted the challenge to get this change to AWS.
-        </a>
-      </header>
-    </div>
-  );
+import { render } from '@testing-library/react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header /> 
+        <Body />    
+        <Footer />
+      </div>
+    );
+  }                       
 }
 
 export default App;
